292a9d28a98330501d30192b6b1fa6839418919d
גוראל בן שבת <jobisraeliim@gmail.com>
2020-09-16 02:41:50 +0300

    Updates

---
+++ precache-manifest.941c04a1c33245072351f1e1354acb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc80032107ece81555492ed00e4e243",
+    "url": "/wa-clone/index.html"
+  },
+  {
+    "revision": "29b4c73d815e838e00da",
+    "url": "/wa-clone/static/css/2.9e944d80.chunk.css"
+  },
+  {
+    "revision": "29b4c73d815e838e00da",
+    "url": "/wa-clone/static/js/2.fbe177f3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wa-clone/static/js/2.fbe177f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed65f4b800cddd295cd",
+    "url": "/wa-clone/static/js/main.e12ee3da.chunk.js"
+  },
+  {
+    "revision": "b43193de5810ce23d9d7",
+    "url": "/wa-clone/static/js/runtime-main.fa8c3eba.js"
+  }
+]);
